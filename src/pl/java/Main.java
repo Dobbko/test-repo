21e8f8be740014da295ac431c0c3bda2a2260c4d
Przemek <przemciu98@tlen.pl>
2021-06-03 21:09:52 +0200

    Klon

--- src/pl/java/Main.java
+++ src/pl/java/Main.java
@@ -2,6 +2,6 @@ package pl.java;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello - Umieść mnie na GitHub'ie");
+        System.out.println("Hello - Jestem klonem");
     }
 }
