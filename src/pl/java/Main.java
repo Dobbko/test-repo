f6c9051c9565309a3ca5d84556d5fc73cc8e5992
Przemek <przemciu98@tlen.pl>
2021-06-03 21:15:52 +0200

    Klon aplikacji

--- src/pl/java/Main.java
+++ src/pl/java/Main.java
@@ -2,6 +2,6 @@ package pl.java;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello - Jestem klonem");
+        System.out.println("Hello - Jestem klonem.");
     }
 }
